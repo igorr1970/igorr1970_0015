cf18b663654628b2b7b98e74dc36def0d8f74f6b
Игорь Рябухин <igorr1970@mail.ru>
2024-11-04 21:16:45 +0500

    Update module_6_1.py

--- module_6_1.py
+++ module_6_1.py
@@ -1,16 +1,14 @@
 class Animal:
     def __init__(self, name):
-        self.alive = True  # Живое
-        self.fed = False   # Накормленное
-        self.name = name   # Имя животного
+        self.alive = True
+        self.fed = False
+        self.name = name
 
-# Базовый класс Plant
 class Plant:
     def __init__(self, name):
-        self.edible = False  # Съедобность
-        self.name = name     # Имя растения
+        self.edible = False
+        self.name = name
 
-# Класс Mammal, наследующий от Animal
 class Mammal(Animal):
     def eat(self, food):
         if food.edible:
@@ -20,7 +18,6 @@ class Mammal(Animal):
             print(f"{self.name} не стал есть {food.name}")
             self.alive = False
 
-# Класс Predator, наследующий от Animal
 class Predator(Animal):
     def eat(self, food):
         if food.edible:
@@ -30,33 +27,28 @@ class Predator(Animal):
             print(f"{self.name} не стал есть {food.name}")
             self.alive = False
 
-# Класс Flower, наследующий от Plant
 class Flower(Plant):
     def __init__(self, name):
         super().__init__(name)
 
-# Класс Fruit, наследующий от Plant и переопределяющий edible
 class Fruit(Plant):
     def __init__(self, name):
         super().__init__(name)
-        self.edible = True  # Переопределяем на съедобное
+        self.edible = True
 
-# Создание объектов классов
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
 p2 = Fruit('Заводной апельсин')
 
-# Проверка состояния объектов и выполнение действий
-print(a1.name)  # Вывод имени хищника
-print(p1.name)  # Вывод имени цветка
+print(a1.name)
+print(p1.name)
 
-print(a1.alive)  # Проверка состояния жизни хищника
-print(a2.fed)    # Проверка состояния накормленности млекопитающего
+print(a1.alive)
+print(a2.fed)
 
-# Попытка покормить животных растениями
-a1.eat(p1)  # Хищник пытается съесть цветок (несъедобное)
-a2.eat(p2)  # Млекопитающее пытается съесть фрукт (съедобное)
+a1.eat(p1)
+a2.eat(p2) 
 
-print(a1.alive)  # Проверка состояния жизни хищника после еды
-print(a2.fed)    # Проверка состояния накормленности млекопитающего после еды
+print(a1.alive) 
+print(a2.fed)
